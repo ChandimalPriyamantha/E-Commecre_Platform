34f9b203a85fb04a95dcacf747a0dadf86653fdd
Lakindu Nethsara <lakindu_2020707@fot.ruh.ac.lk>
2023-03-30 11:28:52 +0530

    Update deleteconsumer.php

--- Admin/deleteconsumer.php
+++ Admin/deleteconsumer.php
@@ -22,6 +22,17 @@
             include "Connection/connection.php";
             $consumerID=$_GET['delete'];
 
+            $check_projects_query=mysqli_query($conn,"select Consumer_ID from project");
+            while ($row=mysqli_fetch_row($check_projects_query)){
+                if($row[0]==$consumerID){
+                    die ('<div class="alert alert-danger" role="alert">
+                        Could not delete this consumer because he has projects and bids...!<br><br>
+                        <a href="consumer.php"><button class="btn btn-primary">Ok</button></a>
+                        </div>');
+                }
+            }
+
+
             $delete_address_query=mysqli_query($conn,"delete from address_of_consumer where Consumer_ID=$consumerID");
             if($delete_address_query){
                 header("Location: consumer.php");
