3d7fab31b56895fc2a56e76fc85521607e703dd3
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2023-04-01 00:08:41 +0530

    registarion page

---
+++ Worker Registrtion/registration.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Register - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aboreto&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Animated-Type-Heading.css">
+    <link rel="stylesheet" href="assets/css/Animated-Typing-With-Blinking.css">
+    <link rel="stylesheet" href="assets/css/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/Cute-Select-width-100.css">
+    <link rel="stylesheet" href="assets/css/Cute-Select.css">
+    <link rel="stylesheet" href="assets/css/Dark-Mode-Switch.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Footer-.css">
+    <link rel="stylesheet" href="assets/css/Team-Horizontal-icons.css">
+    <link rel="stylesheet" href="assets/css/Team-Horizontal-images.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#" style="font-family: Aboreto, serif;">Karmikaya</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="registration.html">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.html">Account</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="page registration-page">
+        <section class="clean-block clean-form dark" style="padding-bottom: 0px;">
+            <div style="height: 1159.59px;position: relative;">
+                <div class="container">
+                    <div class="block-heading">
+                        <h2 class="text-info">Registration</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                    </div>
+                    <form class="shadow" style="height: 680px;width: 1110px;max-width: none;" method="POST">
+                        <div style="width: 255px; float: left;">
+                            <div class="mb-3"><label class="form-label" for="fname">First Name</label><input class="form-control item" type="text" id="name-4" name="fname"></div>
+                            <div class="mb-3"><label class="form-label" for="lname">Last Name</label><input class="form-control item" type="text" id="name-2" name="lname"></div>
+                            <div class="mb-3"><label class="form-label" for="nic">NIC</label><input class="form-control item" type="text" id="name-3" name="nic"></div>
+                            <div class="mb-3"><label class="form-label" for="name">Sex</label>
+                                <select class="form-select" style="width: 255px;border-radius: 6px;height: 38px;" name="sex">
+                                    <optgroup label="Gender">
+                                        <option value="female" selected="">Female</option>
+                                        <option value="male">Male</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                            
+                            <div class="mb-3"><label class="form-label" for="dob">Date of Birth</label><input class="form-control" type="date" name="dob"></div>
+                            <div class="mb-3"><label class="form-label" for="password">Enter Password</label><input class="form-control item" type="password" id="password" name="password"></div>
+                            <div class="mb-3"><label class="form-label" for="password">Enter Again Password</label><input class="form-control item" type="password" id="password-1" name="password-1"></div>
+                        </div>
+                        <div style="width: 255px; float: left; margin-left: 130px;">
+                            <div class="mb-3"><label class="form-label" for="email">Email</label><input class="form-control item" type="email" id="email-1" name="email"></div>
+                            <div class="mb-3"><label class="form-label" for="mnumber">Mobile Number</label><input class="form-control item" type="tel" id="name-4" name="mobileno"></div>
+                            <div class="mb-3"><label class="form-label" for="hnumber">Home Number</label><input class="form-control item" type="tel" id="name-2" name="homeno"></div>
+                            <div class="mb-3"><label class="form-label" for="wpn">Work Place Number</label><input class="form-control item" type="tel" id="name-3" name="workplaceno"></div>
+                            <div class="mb-3"><label class="form-label" for="name">Job Category</label>
+                                <select class="form-select" style="width: 255px;border-radius: 6px;height: 38px;" name="jobcateory">
+                                    <optgroup label="jobc">
+                                        <option value="jb1" selected="">test job 01</option>
+                                        <option value="jb2">test job 02</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                            <div class="mb-3"><label class="form-label" for="recname">Recipient's Name</label><input class="form-control item" type="text" id="recname" name="recepName"></div>
+                            <div class="mb-3"><label class="form-label" for="bsnsname">Business's Name</label><input class="form-control" type="text" id="bsnsname" name="buisnessName"></div>
+                            
+                        </div>
+                        <div style="width: 255px; float: right;">
+                            <div class="mb-3"><label class="form-label" for="zipcode">ZIP Code</label><input class="form-control item" type="number" id="zipnum" name="zip"></div>
+                            <div class="mb-3"><label class="form-label" for="state">State</label><input class="form-control item" type="text" id="state" name="state"></div>
+                            <div class="mb-3"><label class="form-label" for="fname">Country</label><input class="form-control item" type="text" id="name-4" name="country"></div>
+                            <div class="mb-3"><label class="form-label" for="lname">City</label><input class="form-control item" type="text" id="name-2" name="city"></div>
+                            <div class="mb-3"><label class="form-label" for="nic">Street Address</label><input class="form-control item" type="text" id="name-3" name="street"></div>
+                            <button class="btn btn-primary shadow" type="submit" style="margin-top: 11px;" name="signup">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer style="margin-top: 0px;">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 footer-navigation">
+                <h3><a href="#">Company<span>logo </span></a></h3>
+                <p class="company-name">Karmikaya © 2023</p>
+            </div>
+            <div class="col-sm-6 col-md-4 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">21 Revolution Street</span> Paris, France</p>
+                </div>
+                <div><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p class="footer-center-info email text-start"> +1 555 123456</p>
+                </div>
+                <div><i class="fa fa-envelope footer-contacts-icon"></i>
+                    <p> <a href="#" target="_blank">support@company.com</a></p>
+                </div>
+            </div>
+            <div class="col-md-4 footer-about">
+                <h4>About the company</h4>
+                <p> Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet. </p>
+                <div class="social-links social-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/baguetteBox.min.js"></script>
+    <script src="assets/js/vanilla-zoom.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/Animated-Type-Heading-type-headline.js"></script>
+    <script src="assets/js/Animated-Typing-With-Blinking-Animated-Type-Heading.js"></script>
+    <script src="assets/js/Dark-Mode-Switch-darkmode.js"></script>
+
+
+
+
+    <?php
+
+
+    include("conn.php");
+
+    if(isset($_POST["signup"]))
+    {
+        $fname=$_POST['fname'];
+        $lname =$_POST['lname'];
+        $nic=$_POST['nic'];
+        $sex =$_POST['sex'];
+        $dob  =$_POST['dob'];
+        $password =$_POST['password'];
+        $password1 =$_POST['password-1'];
+        $email =$_POST['email'];
+        $mobileno   =$_POST['mobileno'];
+        $homeno   =$_POST['homeno'];
+        $workplaceno=$_POST['workplaceno'];
+        $jobcategory=$_POST['jobcateory'];
+        $recepName=$_POST['recepName'];
+        $buisName=$_POST['buisnessName'];
+        $zip=$_POST['zip'];
+        $state=$_POST['state'];
+        $country =$_POST['country'];
+        $city=$_POST['city'];
+        $street=$_POST['street'];
+       
+    
+
+    if($password==$password1)
+    {
+
+     $workQ=mysqli_query($conn,"INSERT INTO worker (NIC, First_Name, Lat_Name, Sex, Email, DOB, Password,Jobe_Category)
+     VALUES ('$nic','$fname','$lname','$sex','$email','$dob','$password','$jobcategory' )");
+
+     $idQ=mysqli_query($conn,"select ID from worker where NIC='$nic'");
+     while($row = mysqli_fetch_assoc($idQ)) {
+        $id=$row["ID"];
+      }
+
+     $contactQ=mysqli_query($conn,"INSERT INTO `phone_number_of_worker` (`Mobile_Number`, `Home_Number`, `Work_Place_Number`, `Worker_ID`)
+      VALUES ('$mobileno', '$homeno', '$workplaceno','$id');");
+
+      $addressQ=mysqli_query($conn,"INSERT INTO `address_of_worker` (`Recipient's name`, `Business's name`, ` ZIP code`, `State`, `Country`, `City`, `Street_address`, `Worker_ID`) 
+      VALUES ('$recepName', '$buisName', '$zip', '$state', '$country', '$city', '$street', '$id');");
+
+    }
+
+    else
+    {
+        //error message passwords are not matching
+
+    }
+}
+
+
+
+
+
+
+    ?>
+
+
+
+
+
+
+</body>
+
+</html>
